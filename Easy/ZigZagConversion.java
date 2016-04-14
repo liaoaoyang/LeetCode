e84147586513b6b8d2155dd7a83f03dfde46c272
liaoaoyang <liaoaoyang@gmail.com>
2016-04-14 16:03:04 +0800

    Solved Easy 6

--- Easy/ZigZagConversion.java
+++ Easy/ZigZagConversion.java
@@ -2,65 +2,31 @@
  * Created by aoyang on 16/4/14.
  */
 public class ZigZagConversion {
-    public static final int STAGE_COL    = 0;
-    public static final int STAGE_ZIGZAG = 1;
-    
     public String convert(String s, int numRows) {
         if (numRows == 1) {
             return s;
         }
 
         int sLen = s.length();
-        int zigZagLen = (numRows - 2 >= 0 ? numRows - 2 : 0);
-
-        String r = "";
-        int stage = 0;
-        int zigZagIdx = 1;
-        int colIdx = 0;
-
-        for (int i = 0; i < sLen; ++i) {
-            String nowStr = s.substring(i, i + 1);
-            if (stage == STAGE_COL) {
-                r = r + nowStr;
-                ++colIdx;
-
-                if (colIdx > 0 && colIdx % numRows == 0) {
-                    stage = STAGE_ZIGZAG;
-                    zigZagIdx = 1;
-                }
-            } else {
-                if (zigZagLen == 0) {
-                    stage = STAGE_COL;
-                    colIdx = 0;
-                    continue;
-                }
-
-                for (int j = 0; j < numRows; ++j) {
-                    if (j != zigZagIdx) {
-                        r = r + "#";
-                        continue;
-                    }
-
-                    r = r + nowStr;
-                }
-
-                ++zigZagIdx;
-
-                if (zigZagIdx > zigZagLen) {
-                    stage = STAGE_COL;
-                    colIdx = 0;
-                }
-            }
-        }
+        int colNum = (int) Math.ceil(1.0 * sLen / numRows);
+        int sectionNum = 2 * numRows - 2;
 
         String result = "";
-        int rLen = r.length();
 
-        for (int row = 0; row < numRows; ++ row) {
-            for (int i = row; i < rLen; i += numRows) {
-                String nowStr = r.substring(i, i + 1);
-                if (!nowStr.equals("#")) {
-                    result = result + nowStr;
+        for (int r = 0; r < numRows; ++r) {
+            for (int c = 0; c < colNum; ++c) {
+                int idx = c * sectionNum + r;
+
+                if (idx < sLen) {
+                    result += s.substring(idx, idx + 1);
+
+                    if (r >= 1 && r <= numRows - 2) {
+                        int idxAppend = (c + 1) * sectionNum - r;
+
+                        if (idxAppend >= 0 && idxAppend < sLen) {
+                            result += s.substring(idxAppend, idxAppend + 1);
+                        }
+                    }
                 }
             }
         }
