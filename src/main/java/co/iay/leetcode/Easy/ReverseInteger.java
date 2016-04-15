89550286f6b048a77f953030872563aaec3de416
liaoaoyang <liaoaoyang@gmail.com>
2016-04-16 02:04:28 +0800

    Brute force to solve Easy 7

---
+++ src/main/java/co/iay/leetcode/Easy/ReverseInteger.java
@@ -0,0 +1,29 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by ng on 16/4/16.
+ */
+public class ReverseInteger {
+    public int reverse(int x) {
+        StringBuffer sb = new StringBuffer(x + "");
+        Long xL;
+        int factor = 1;
+
+        if (sb.substring(0, 1).equals("-")) {
+            factor = -1;
+            xL = Long.parseLong(sb.replace(0, 1, "").reverse().toString());
+        } else {
+            xL = Long.parseLong(sb.reverse().toString());
+        }
+
+        if (factor > 0 && xL > Integer.MAX_VALUE) {
+            return 0;
+        }
+
+        if (factor < 0 && (-1 * xL) < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return xL.intValue() * factor;
+    }
+}
