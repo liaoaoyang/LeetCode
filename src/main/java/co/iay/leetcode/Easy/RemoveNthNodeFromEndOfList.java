8df59efc8b66a39813b27a47c138c160a4c88ef9
liaoaoyang <liaoaoyang@gmail.com>
2016-04-23 23:50:22 +0800

    Solved Easy 19

---
+++ src/main/java/co/iay/leetcode/Easy/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,41 @@
+package co.iay.leetcode.Easy;
+
+import co.iay.leetcode.DataStructures.ListNode;
+
+import java.util.HashMap;
+
+/**
+ * Created by ng on 16/4/23.
+ */
+public class RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        HashMap<Integer, ListNode> idxListMap = new HashMap<Integer, ListNode>();
+
+        ListNode it = head;
+        int i = 1;
+
+        for(; it != null; it = it.next, ++i) {
+            idxListMap.put(i, it);
+        }
+
+        if (idxListMap.size() == 1) {
+            return null;
+        }
+
+        if (n == 1) {
+            idxListMap.get(idxListMap.size() - 1).next = null;
+            return head;
+        }
+
+        if (n == idxListMap.size()) {
+            head = head.next;
+            return head;
+        }
+
+        if (idxListMap.containsKey(idxListMap.size() - n) && idxListMap.containsKey(idxListMap.size() - n + 2)) {
+            idxListMap.get(idxListMap.size() - n).next = idxListMap.get(idxListMap.size() - n + 2);
+        }
+
+        return head;
+    }
+}
