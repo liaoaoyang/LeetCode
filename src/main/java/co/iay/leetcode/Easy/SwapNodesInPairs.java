50e9949b7a7a744ec8b9aa69b003d8d8936eea64
liaoaoyang <liaoaoyang@gmail.com>
2016-05-01 20:31:25 +0800

    Brute force Easy 24

---
+++ src/main/java/co/iay/leetcode/Easy/SwapNodesInPairs.java
@@ -0,0 +1,44 @@
+package co.iay.leetcode.Easy;
+
+import co.iay.leetcode.DataStructures.ListNode;
+
+/**
+ * Created by ng on 16/5/1.
+ */
+public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        ListNode firstOfPair = null;
+        ListNode nowNode = head;
+        ListNode privNode = null;
+        int i = 1;
+
+        while (nowNode != null) {
+            if (i % 2 == 1) {
+                firstOfPair = nowNode;
+                nowNode = nowNode.next;
+            } else {
+                if (firstOfPair == null) {
+                    return head;
+                }
+
+                if (privNode != null) {
+                    privNode.next = nowNode;
+                }
+
+                privNode = firstOfPair;
+                firstOfPair.next = nowNode.next;
+                nowNode.next = firstOfPair;
+
+                if (i == 2) {
+                    head = nowNode;
+                }
+
+                nowNode = firstOfPair.next;
+            }
+
+            ++i;
+        }
+
+        return head;
+    }
+}
