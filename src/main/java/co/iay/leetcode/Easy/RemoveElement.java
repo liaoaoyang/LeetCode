c2c31e6dc3a746fe029f5c05644318e38f6e1ca0
liaoaoyang <liaoaoyang@gmail.com>
2016-05-01 17:13:11 +0800

    Brute force Easy 27

---
+++ src/main/java/co/iay/leetcode/Easy/RemoveElement.java
@@ -0,0 +1,23 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by ng on 16/4/30.
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int removed = 0;
+
+        for (int i = 0; i < nums.length - removed; ++i) {
+            if (nums[i] == val) {
+                for (int j = i; j + 1 < nums.length; ++j) {
+                    nums[j] = nums[j + 1];
+                }
+
+                ++removed;
+                --i;
+            }
+        }
+
+        return nums.length - removed;
+    }
+}
