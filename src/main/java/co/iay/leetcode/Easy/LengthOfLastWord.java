a796fb816cc84d37f3d0ea73d36b49fc3984ea0b
liaoaoyang <liaoaoyang@gmail.com>
2016-05-06 17:07:39 +0800

    Brute force Easy 58

---
+++ src/main/java/co/iay/leetcode/Easy/LengthOfLastWord.java
@@ -0,0 +1,27 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by aoyang on 16/5/6.
+ */
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        boolean canCount = false;
+        int len = 0;
+
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if (s.substring(i, i + 1).equals(" ")) {
+                if (canCount) {
+                    return len;
+                }
+            } else {
+                if (!canCount) {
+                    canCount = true;
+                }
+
+                ++len;
+            }
+        }
+
+        return len;
+    }
+}
