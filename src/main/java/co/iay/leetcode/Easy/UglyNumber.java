246a8f8bf6ca9602eb4ca24d9a1093e17aa37396
liaoaoyang <liaoaoyang@gmail.com>
2016-05-08 22:50:09 +0800

    Brute force Easy 263

---
+++ src/main/java/co/iay/leetcode/Easy/UglyNumber.java
@@ -0,0 +1,32 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by ng on 16/5/8.
+ */
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num <= 0) {
+            return false;
+        }
+
+        if (1 == num) {
+            return true;
+        }
+
+        int[] factors = {2, 3, 5};
+
+        int n = num;
+
+        for (int factor: factors) {
+            while (0 == n % factor) {
+                n /= factor;
+
+                if (n == 1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
