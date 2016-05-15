f7a470019d0c309527f6b26858e7ea4dbd9326d3
liaoaoyang <liaoaoyang@gmail.com>
2016-05-15 23:49:21 +0800

    Try RectangleArea

---
+++ src/main/java/co/iay/leetcode/Easy/RectangleArea.java
@@ -0,0 +1,10 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by ng on 16/5/15.
+ */
+public class RectangleArea {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        return 0;
+    }
+}
