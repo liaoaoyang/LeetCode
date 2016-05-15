2bb8c5a61fa5c95d572e894db4e15be5547b82c3
liaoaoyang <liaoaoyang@gmail.com>
2016-05-16 01:09:30 +0800

    Brute force Easy 223

--- src/main/java/co/iay/leetcode/Easy/RectangleArea.java
+++ src/main/java/co/iay/leetcode/Easy/RectangleArea.java
@@ -5,6 +5,25 @@ package co.iay.leetcode.Easy;
  */
 public class RectangleArea {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        return 0;
+        int totalArea = (C - A) * (D - B) + (G - E) * (H - F);
+
+        if (A <= E && B <= F && C >= G && D >= H) {
+            return (C - A) * (D - B);
+        }
+
+        if (A > E && B > F && C < G && D < H) {
+            return (G - E) * (H - F);
+        }
+
+        if (E >= C || H <= B || G <= A || F >= D) {
+            return totalArea;
+        }
+
+        int leftX = A > E ? A : E;
+        int leftY = B > F ? B : F;
+        int rightX = C > G ? G : C;
+        int rightY = D > H ? H : D;
+
+        return totalArea - (rightX - leftX) * (rightY - leftY);
     }
 }
