42701e3ef3a0c6e845c3d794a09507f19ac8b887
liaoaoyang <liaoaoyang@gmail.com>
2016-05-24 21:31:44 +0800

    Brute force Easy 349

---
+++ src/main/java/co/iay/leetcode/Easy/IntersectionOfTwoArrays.java
@@ -0,0 +1,39 @@
+package co.iay.leetcode.Easy;
+
+import java.util.HashMap;
+
+/**
+ * Created by ng on 16/5/24.
+ */
+public class IntersectionOfTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int[] longer = nums1.length > nums2.length ? nums1 : nums2;
+        int[] shorter = nums1.length <= nums2.length ? nums1 : nums2;
+        HashMap<Integer, Integer> r = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> n1 = new HashMap<Integer, Integer>();
+
+        for (int i: longer) {
+            n1.put(i, 1);
+        }
+
+        for (int i: shorter) {
+            if (n1.containsKey(i)) {
+                r.put(i, 1);
+            }
+        }
+
+        if (r.size() == 0) {
+            return new int[0];
+        }
+
+        int[] result = new int[r.size()];
+        int idx = 0;
+
+        for (Integer i : r.keySet()) {
+            result[idx] = i;
+            ++idx;
+        }
+
+        return result;
+    }
+}
