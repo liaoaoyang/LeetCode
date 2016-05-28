df01ef1e66b175f85bbd56615f2b2ac515ed80e5
liaoaoyang <liaoaoyang@gmail.com>
2016-05-28 16:31:21 +0800

    Cache Result to solve Easy 303

--- src/main/java/co/iay/leetcode/Easy/RangeSumQuery.java
+++ src/main/java/co/iay/leetcode/Easy/RangeSumQuery.java
@@ -5,18 +5,19 @@ package co.iay.leetcode.Easy;
  */
 public class RangeSumQuery {
     int[] nums = null;
+    int[] result = null;
 
     public RangeSumQuery(int[] nums) {
         this.nums = nums;
+        this.result = new int[nums.length + 1];
+        this.result[0] = 0;
+
+        for (int end = 1; end <= nums.length; ++end) {
+            this.result[end] = this.result[end - 1] + this.nums[end - 1];
+        }
     }
 
     public int sumRange(int i, int j) {
-        int sum = 0;
-
-        for (int idx = i; idx <= j; ++idx) {
-            sum += this.nums[idx];
-        }
-
-        return sum;
+        return this.result[j + 1] - this.result[i];
     }
 }
