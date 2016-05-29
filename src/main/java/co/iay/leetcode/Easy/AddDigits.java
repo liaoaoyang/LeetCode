c2635ba390e1e2d5bd42396934108d6c8c64ac02
liaoaoyang <liaoaoyang@gmail.com>
2016-05-29 17:32:54 +0800

    Brute force Easy 258

---
+++ src/main/java/co/iay/leetcode/Easy/AddDigits.java
@@ -0,0 +1,23 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by ng on 16/5/29.
+ */
+public class AddDigits {
+    public int addDigits(int num) {
+        int now = num;
+        int result = 0;
+
+        while (now >= 10){
+            while (now > 0) {
+                result += (now % 10);
+                now /= 10;
+            }
+
+            now = result;
+            result = 0;
+        }
+
+        return now;
+    }
+}
