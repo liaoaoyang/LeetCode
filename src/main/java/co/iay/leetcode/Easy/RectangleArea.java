a65476283b639c459013641deee853f05861cc1b
liaoaoyang <liaoaoyang@gmail.com>
2016-06-02 00:25:57 +0800

    Add link

--- src/main/java/co/iay/leetcode/Easy/RectangleArea.java
+++ src/main/java/co/iay/leetcode/Easy/RectangleArea.java
@@ -2,6 +2,7 @@ package co.iay.leetcode.Easy;
 
 /**
  * Created by ng on 16/5/15.
+ * https://leetcode.com/problems/rectangle-area/
  */
 public class RectangleArea {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
