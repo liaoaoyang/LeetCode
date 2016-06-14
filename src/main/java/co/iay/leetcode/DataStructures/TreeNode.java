cacd324c613f6cd112b10d6c0c9b46a0b6992a0f
liaoaoyang <liaoaoyang@gmail.com>
2016-06-14 22:39:37 +0800

    Add Class TreeNode

---
+++ src/main/java/co/iay/leetcode/DataStructures/TreeNode.java
@@ -0,0 +1,12 @@
+package co.iay.leetcode.DataStructures;
+
+/**
+ * Created by ng on 16/6/13.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) { val = x; }
+}
