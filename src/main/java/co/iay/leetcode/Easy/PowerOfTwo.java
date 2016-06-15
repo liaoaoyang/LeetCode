70a9bd672c27fd06e48921e4000466a2adf2c660
liaoaoyang <liaoaoyang@gmail.com>
2016-06-16 00:09:31 +0800

    Brute force Easy 231

---
+++ src/main/java/co/iay/leetcode/Easy/PowerOfTwo.java
@@ -0,0 +1,22 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by ng on 16/6/16.
+ */
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n > 1) {
+            if (n % 2 == 1) {
+                return false;
+            }
+
+            n /= 2;
+        }
+
+        return true;
+    }
+}
