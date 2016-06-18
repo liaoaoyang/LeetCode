a5b9611f8a43e90e2a0759da09eed67bc7f8ea36
liaoaoyang <liaoaoyang@gmail.com>
2016-06-18 10:44:43 +0800

    Brute force Easy 291

---
+++ src/main/java/co/iay/leetcode/Easy/NumberOf1Bits.java
@@ -0,0 +1,30 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by ng on 16/6/18.
+ */
+public class NumberOf1Bits {
+    public int hammingWeight(int n) {
+        long unsignedN = n;
+
+        if (unsignedN < 0) {
+            unsignedN += 4294967296L;
+        }
+
+        int num = 0;
+        int maxPos = 31;
+
+        while (unsignedN > 0) {
+            for (int pos = maxPos; pos >= 0; --pos) {
+                if (((long)1<<pos) <= unsignedN) {
+                    ++num;
+                    unsignedN -= ((long)1<<pos);
+                    maxPos = pos - 1;
+                    break;
+                }
+            }
+        }
+
+        return num;
+    }
+}
