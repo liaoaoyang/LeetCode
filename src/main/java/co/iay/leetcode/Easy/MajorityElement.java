f086d9fdc82b9aa1de4555b9143ac127826e4f87
liaoaoyang <liaoaoyang@gmail.com>
2016-06-24 21:29:55 +0800

    Brute force Easy 169

---
+++ src/main/java/co/iay/leetcode/Easy/MajorityElement.java
@@ -0,0 +1,28 @@
+package co.iay.leetcode.Easy;
+
+import java.util.HashMap;
+
+/**
+ * Created by ng on 16/6/24.
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+
+        for (int i : nums) {
+            if (m.containsKey(i)) {
+                m.put(i, m.get(i) + 1);
+            } else {
+                m.put(i, 1);
+            }
+        }
+
+        for (Integer k : m.keySet()) {
+            if (m.get(k) > (int)Math.floor(nums.length / 2.0)) {
+                return k;
+            }
+        }
+
+        return 0;
+    }
+}
