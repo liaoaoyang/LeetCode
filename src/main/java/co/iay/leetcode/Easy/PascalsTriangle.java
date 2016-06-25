52e2330b522b768d22dced24cefbc750cc058130
liaoaoyang <liaoaoyang@gmail.com>
2016-06-25 10:40:37 +0800

    Brute force Easy 118

---
+++ src/main/java/co/iay/leetcode/Easy/PascalsTriangle.java
@@ -0,0 +1,40 @@
+package co.iay.leetcode.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ng on 16/6/24.
+ */
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        if (numRows <= 0) {
+            return result;
+        }
+
+        List<Integer> prev = null;
+
+        for (int i = 1; i <= numRows; ++i) {
+            List<Integer> now = new ArrayList<Integer>();
+            now.add(1);
+
+            if (prev == null) {
+                result.add(now);
+                prev = now;
+                continue;
+            }
+
+            for (int j = 0 ; j < prev.size() - 1; ++j) {
+                now.add(prev.get(j) + prev.get(j + 1));
+            }
+
+            now.add(1);
+            result.add(now);
+            prev = now;
+        }
+
+        return result;
+    }
+}
