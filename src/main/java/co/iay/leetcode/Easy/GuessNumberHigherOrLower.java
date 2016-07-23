dc506bf5c4e9ac66a0552023c12b9f1ca80cce86
liaoaoyang <liaoaoyang@gmail.com>
2016-07-23 21:41:44 +0800

    Brute force Easy 374

--- src/main/java/co/iay/leetcode/Easy/GuessNumberHigherOrLower.java
+++ src/main/java/co/iay/leetcode/Easy/GuessNumberHigherOrLower.java
@@ -11,13 +11,13 @@ public class GuessNumberHigherOrLower extends GuessGame {
         int end = n;
 
         while (start <= end) {
-            int now = (start + end) / 2;
+            int now = start + (end - start) / 2;
             int r = this.guess(now);
 
             if (r == 1) {
-                end = now - 1;
-            } else if (r == -1) {
                 start = now + 1;
+            } else if (r == -1) {
+                end = now - 1;
             } else {
                 return now;
             }
