0a2e4083f7c1028837c0e9c58f9145121cb7363c
liaoaoyang <liaoaoyang@gmail.com>
2016-08-11 23:39:48 +0800

    Add test case for power of four

---
+++ src/main/java/co/iay/leetcode/Tester/Easy/TestPowerOfFour.java
@@ -0,0 +1,24 @@
+package co.iay.leetcode.Tester.Easy;
+
+import co.iay.leetcode.Easy.PowerOfFour;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by ng on 16/8/11.
+ */
+public class TestPowerOfFour {
+    private PowerOfFour powerOfFour = new PowerOfFour();
+
+    @Test
+    public void testPowerOfFourCase1()
+    {
+        Assert.assertEquals(true, powerOfFour.isPowerOfFour(1<<2));
+    }
+
+    @Test
+    public void testPowerOfFourCase2()
+    {
+        Assert.assertEquals(false, powerOfFour.isPowerOfFour((1<<1) + 1));
+    }
+}
