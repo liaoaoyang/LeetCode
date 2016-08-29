40554216a123a7f42b61198bfc3f567ffeeb2a42
liaoaoyang <liaoaoyang@gmail.com>
2016-08-29 23:56:17 +0800

    Brute force easy 389

---
+++ src/main/java/co/iay/leetcode/Easy/FindTheDifference.java
@@ -0,0 +1,38 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by ng on 16/8/29.
+ */
+public class FindTheDifference {
+    private static int LEN = 256;
+
+    public char findTheDifference(String s, String t) {
+        int[] ss = new int[LEN];
+        int[] tt = new int[LEN];
+
+        for (int i = 0; i < LEN; ++i) {
+            ss[i] = 0;
+            tt[i] = 0;
+        }
+
+        for (int i = 0; i < s.length(); ++i) {
+            int code = (int)(s.charAt(i) - '\0');
+
+            ++ss[code];
+        }
+
+        for (int i = 0; i < t.length(); ++i) {
+            int code = (int)(t.charAt(i) - '\0');
+
+            ++tt[code];
+        }
+
+        for (int i = 0; i < LEN; ++i) {
+            if (ss[i] != tt[i]) {
+                return (char)i;
+            }
+        }
+
+        return '\0';
+    }
+}
