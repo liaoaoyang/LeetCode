5e662169b989ff8f6fc7e5f20792c897daf2cd01
liaoaoyang <liaoaoyang@gmail.com>
2016-09-04 19:12:15 +0800

    Brute force easy 136

---
+++ src/main/java/co/iay/leetcode/Easy/SingleNumber.java
@@ -0,0 +1,30 @@
+package co.iay.leetcode.Easy;
+
+import java.util.HashMap;
+
+/**
+ * Created by ng on 16/9/4.
+ */
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i < nums.length; ++i) {
+            if (m.containsKey(nums[i])) {
+                m.put(nums[i], m.get(nums[i]) + 1);
+                continue;
+            }
+
+            m.put(nums[i], 1);
+
+        }
+
+        for(int i = 0; i < nums.length; ++i) {
+            if (m.get(nums[i]) == 1) {
+                return nums[i];
+            }
+        }
+
+        return 0;
+    }
+}
