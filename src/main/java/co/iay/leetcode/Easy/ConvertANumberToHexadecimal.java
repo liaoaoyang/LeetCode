2acc3c9482fa0e2e22c206b5351af4fb895e3978
liaoaoyang <liaoaoyang@gmail.com>
2016-10-01 23:59:19 +0800

    Brute force easy 409

---
+++ src/main/java/co/iay/leetcode/Easy/ConvertANumberToHexadecimal.java
@@ -0,0 +1,25 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by ng on 16/10/1.
+ */
+public class ConvertANumberToHexadecimal {
+    public String toHex(int num) {
+        long n = num >= 0 ? num : num + 0x100000000L;
+        String hex = "";
+
+        while (n > 0) {
+            long mod = n % 16;
+
+            if (n < 10) {
+                hex = mod + hex;
+            } else {
+                hex = (char)((mod - 10) + 'a') + hex;
+            }
+
+            n /= 16;
+        }
+
+        return hex.isEmpty() ? "0" : hex;
+    }
+}
