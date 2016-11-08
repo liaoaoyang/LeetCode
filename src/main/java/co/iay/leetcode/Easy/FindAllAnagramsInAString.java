6e274933dc64a68c0c96b2884bd9df609f37a0d5
liaoaoyang <liaoaoyang@gmail.com>
2016-11-08 23:58:52 +0800

    Brute force easy 438

---
+++ src/main/java/co/iay/leetcode/Easy/FindAllAnagramsInAString.java
@@ -0,0 +1,48 @@
+package co.iay.leetcode.Easy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by ng on 16/11/8.
+ */
+public class FindAllAnagramsInAString {
+    public List<Integer> findAnagrams(String s, String p) {
+        if (s.length() < p.length()) {
+            return null;
+        }
+
+        List<Integer> result = new ArrayList<Integer>();
+
+        HashMap<String, Boolean> pMap = new HashMap<String, Boolean>();
+
+        for (int i = 0; i < p.length(); ++i) {
+            pMap.put(p.charAt(i) + "", true);
+        }
+
+        for (int i = 0; i < s.length() - p.length(); ++i) {
+            String sChar = s.charAt(i) + "";
+
+            if (!pMap.containsKey(sChar)) {
+                continue;
+            }
+
+            HashMap<String, Boolean> tpMap = new HashMap<String, Boolean>();
+
+            for (int j = i; j < i + p.length(); ++j) {
+                String jChar = s.charAt(j) + "";
+
+                if (pMap.containsKey(jChar)) {
+                    tpMap.put(jChar, true);
+                }
+            }
+
+            if (tpMap.keySet().size() == pMap.keySet().size()) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/co/iay/leetcode/Tester/Easy/TestFindAllAnagramsInAString.java
@@ -0,0 +1,23 @@
+package co.iay.leetcode.Tester.Easy;
+
+import co.iay.leetcode.Easy.FindAllAnagramsInAString;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ng on 16/11/8.
+ */
+public class TestFindAllAnagramsInAString {
+    private FindAllAnagramsInAString t = new FindAllAnagramsInAString();
+
+    @Test
+    public void testFindAllAnagramsInAStringCase1() {
+        List<Integer> r = new ArrayList<Integer>();
+        r.add(0);
+        r.add(6);
+        Assert.assertEquals(r, t.findAnagrams("cbaebabacd", "abc"));
+    }
+}
