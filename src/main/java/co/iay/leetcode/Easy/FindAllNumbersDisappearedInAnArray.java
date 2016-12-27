83a6ca4f7edcd0644ef06b26cf95eab46998e4c4
liaoaoyang <liaoaoyang@gmail.com>
2016-12-27 16:29:00 +0800

    Easy 448 sloved, used extra spaces

--- src/main/java/co/iay/leetcode/Easy/FindAllNumbersDisappearedInAnArray.java
+++ src/main/java/co/iay/leetcode/Easy/FindAllNumbersDisappearedInAnArray.java
@@ -1,7 +1,6 @@
 package co.iay.leetcode.Easy;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -9,23 +8,21 @@ import java.util.List;
  */
 public class FindAllNumbersDisappearedInAnArray {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        int[] rNums = new int[nums.length];
+
+        for (int i = 1; i <= rNums.length; ++i) {
+            rNums[i - 1] = i;
+        }
+
+        for (int n: nums) {
+            rNums[n - 1] = 0;
+        }
+
         List<Integer> r = new ArrayList<Integer>();
 
-        for (int i = 1; i <= nums.length; ++i) {
-            r.add(i);
-        }
-
-        for (int i : nums) {
-            r.set(i - 1, 0);
-        }
-
-        Iterator<Integer> it = r.iterator();
-
-        while (it.hasNext()) {
-            Integer v = it.next();
-
-            if (v == 0) {
-                it.remove();
+        for (int i: rNums) {
+            if (i != 0) {
+                r.add(i);
             }
         }
 
