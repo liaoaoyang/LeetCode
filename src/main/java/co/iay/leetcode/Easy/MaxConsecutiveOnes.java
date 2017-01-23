04c1506713b152923d1265c57d14e094b53d53cd
liaoaoyang <liaoaoyang@gmail.com>
2017-01-23 20:31:57 +0800

    Solved Easy 485

---
+++ src/main/java/co/iay/leetcode/Easy/MaxConsecutiveOnes.java
@@ -0,0 +1,39 @@
+package co.iay.leetcode.Easy;
+
+/**
+ * Created by ng on 17/1/23.
+ */
+public class MaxConsecutiveOnes {
+    private static int STATUS_ZERO = 0;
+    private static int STATUS_ONE  = 1;
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int nowConsecutiveOnesCount = 0;
+        int maxConsecutiveOnesCount = 0;
+        int status = STATUS_ZERO;
+
+        for (int i : nums) {
+            if (status == STATUS_ZERO && i == 1) {
+                status = STATUS_ONE;
+                nowConsecutiveOnesCount = 1;
+                continue;
+            }
+
+            if (status == STATUS_ONE) {
+                if (i == 1) {
+                    ++nowConsecutiveOnesCount;
+                } else {
+                    status = STATUS_ZERO;
+
+                    if (nowConsecutiveOnesCount > maxConsecutiveOnesCount) {
+                        maxConsecutiveOnesCount = nowConsecutiveOnesCount;
+                    }
+
+                    nowConsecutiveOnesCount = 0;
+                }
+            }
+        }
+
+        return nowConsecutiveOnesCount > maxConsecutiveOnesCount ? nowConsecutiveOnesCount : maxConsecutiveOnesCount;
+    }
+}
---
+++ src/main/java/co/iay/leetcode/Tester/Easy/TestMaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+package co.iay.leetcode.Tester.Easy;
+
+import co.iay.leetcode.Easy.MaxConsecutiveOnes;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by ng on 17/1/23.
+ */
+public class TestMaxConsecutiveOnes {
+    private MaxConsecutiveOnes m = new MaxConsecutiveOnes();
+
+    @Test
+    public void testMaxConsecutiveOnesCase1() {
+        Assert.assertEquals(3, m.findMaxConsecutiveOnes(new int[] {1,1,0,1,1,1}));
+    }
+
+    @Test
+    public void testMaxConsecutiveOnesCase2() {
+        Assert.assertEquals(4, m.findMaxConsecutiveOnes(new int[] {1,1,0,1,1,1,0,1,1,0,1,1,1,1,0,1,1,1}));
+    }
+}
