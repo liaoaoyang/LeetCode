a0db560406b90a17cfd209172da8b82829aabbbb
liaoaoyang <liaoaoyang@gmail.com>
2017-01-29 00:03:24 +0800

    Simplify Easy 463 solution

--- src/main/java/co/iay/leetcode/Easy/IslandPerimeter.java
+++ src/main/java/co/iay/leetcode/Easy/IslandPerimeter.java
@@ -4,51 +4,41 @@ package co.iay.leetcode.Easy;
  * Created by ng on 17/1/28.
  */
 public class IslandPerimeter {
-    public int islandPerimeter(int[][] grid) {
+    private static int DIR_HORIZONTAL = 0;
+    private static int DIR_VERTICAL   = 1;
+
+    private static int countWithDirection(int[][] grid, int dir) {
         int result = 0;
-        int hPrev = 0;
-        int colCount = 0;
-        int rowCount = grid.length;
+        int prev = 0;
+        int iCount = (dir == DIR_HORIZONTAL ? grid.length : grid[0].length);
+        int jCount = (dir == DIR_HORIZONTAL ? grid[0].length : grid.length);
 
-        for (int i = 0; i < rowCount; ++i) {
-            colCount = grid[i].length;
-            hPrev = 0;
+        for (int i = 0; i < iCount; ++i) {
+            prev = 0;
 
-            for (int j = 0; j < colCount; ++j) {
-                int hg = grid[i][j];
+            for (int j = 0; j < jCount; ++j) {
+                int g = (dir == DIR_HORIZONTAL ? grid[i][j] : grid[j][i]);
 
-                if (hg != hPrev) {
+                if (g != prev) {
                     ++result;
                 }
 
-                if (j == colCount - 1 && hg == 1) {
+                if (j == jCount - 1 && g == 1) {
                     ++result;
                 }
 
-                hPrev = hg;
-            }
-        }
-
-        int vPrev = 0;
-
-        for (int k = 0; k < colCount; ++k) {
-            vPrev = 0;
-
-            for (int l = 0; l < rowCount; ++l) {
-                int vg = grid[l][k];
-
-                if (vg != vPrev) {
-                    ++result;
-                }
-
-                if (l == rowCount - 1 && vg == 1) {
-                    ++result;
-                }
-
-                vPrev = vg;
+                prev = g;
             }
         }
 
         return result;
+    }
+
+    public int islandPerimeter(int[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+
+        return countWithDirection(grid, DIR_HORIZONTAL) + countWithDirection(grid, DIR_VERTICAL);
     }
 }
