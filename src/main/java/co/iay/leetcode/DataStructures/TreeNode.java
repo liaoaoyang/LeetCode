b2004b33bf2e814424192b17fd14efa0a0fd32e1
liaoaoyang <liaoaoyang@gmail.com>
2017-02-13 08:28:36 +0800

    Build tree from array

--- src/main/java/co/iay/leetcode/DataStructures/TreeNode.java
+++ src/main/java/co/iay/leetcode/DataStructures/TreeNode.java
@@ -8,5 +8,50 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
-    TreeNode(int x) { val = x; }
+    TreeNode(int x) {
+        val = x;
+    }
+
+    public static TreeNode buildTree(Integer[] list) {
+        if (list.length == 0) {
+            return null;
+        }
+
+        TreeNode[] tmpTree = new TreeNode[list.length];
+
+        for (int i = 0; i < list.length; ++i) {
+            if (list[i] == null) {
+                continue;
+            }
+
+            tmpTree[i] = new TreeNode(list[i]);
+        }
+
+        int nonEmpty = 0;
+
+        for (int i = 0; i < tmpTree.length; ++i) {
+            if (tmpTree[i] == null) {
+                continue;
+            }
+
+            ++nonEmpty;
+            int leftIdx = i * 2 + 1;
+            int rightIdx = i * 2 + 2;
+            tmpTree[i].left = (leftIdx < tmpTree.length && tmpTree[leftIdx] != null ? tmpTree[leftIdx] : null);
+            tmpTree[i].right = (rightIdx < tmpTree.length && tmpTree[rightIdx] != null ? tmpTree[rightIdx] : null);
+        }
+
+        TreeNode[] tree = new TreeNode[nonEmpty];
+
+        for (int i = 0, j = 0; i < tmpTree.length; ++i) {
+            if (tmpTree[i] == null) {
+                continue;
+            }
+
+            tree[j] = tmpTree[i];
+            ++j;
+        }
+
+        return tree[0];
+    }
 }
