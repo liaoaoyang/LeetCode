9d89eafc0fb9a6652c2a3b2d7b974cf1382d59ae
liaoaoyang <liaoaoyang@gmail.com>
2017-03-26 21:44:33 +0800

    Solved Medium 515

---
+++ src/main/java/co/iay/leetcode/Medium/FindBottomLeftTreeValue.java
@@ -0,0 +1,12 @@
+package co.iay.leetcode.Medium;
+
+import co.iay.leetcode.DataStructures.TreeNode;
+
+/**
+ * Created by ng on 17/3/26.
+ */
+public class FindBottomLeftTreeValue {
+    public int findBottomLeftValue(TreeNode root) {
+        return 0;
+    }
+}
