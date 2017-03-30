b17767d44c65f2fe7ebb4712cec0707290bad461
liaoaoyang <liaoaoyang@gmail.com>
2017-03-30 22:18:05 +0800

    Solved Medium 199

---
+++ src/main/java/co/iay/leetcode/Medium/BinaryTreeRightSideView.java
@@ -0,0 +1,57 @@
+package co.iay.leetcode.Medium;
+
+import co.iay.leetcode.DataStructures.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ng on 17/3/30.
+ * 199. Binary Tree Right Side View
+ * https://leetcode.com/problems/binary-tree-right-side-view
+ */
+public class BinaryTreeRightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        if (root == null) {
+            return result;
+        }
+
+        List<TreeNode> l = new ArrayList<TreeNode>();
+        l.add(root);
+        result.add(root.val);
+
+        while (true) {
+            List<TreeNode> pl = l;
+            l = new ArrayList<TreeNode>();
+
+            boolean allNull = true;
+
+            for(TreeNode node : pl) {
+                if (node == null) {
+                    continue;
+                }
+
+                if (node.left != null) {
+                    l.add(node.left);
+                    allNull = false;
+                }
+
+                if (node.right != null) {
+                    l.add(node.right);
+                    allNull = false;
+                }
+            }
+
+            if (allNull) {
+                break;
+            }
+            else {
+                result.add(l.get(l.size() - 1).val);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/co/iay/leetcode/Tester/Medium/TestBinaryTreeRightSideView.java
@@ -0,0 +1,33 @@
+package co.iay.leetcode.Tester.Medium;
+
+import co.iay.leetcode.DataStructures.TreeNode;
+import co.iay.leetcode.Medium.BinaryTreeRightSideView;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by ng on 17/3/30.
+ */
+public class TestBinaryTreeRightSideView {
+    private BinaryTreeRightSideView b = new BinaryTreeRightSideView();
+
+    @Test
+    public void testBinaryTreeRightSideViewCase1() {
+        TreeNode t = TreeNode.buildTree(new Integer[] {});
+        Assert.assertEquals(Arrays.asList(new Integer[] {}), b.rightSideView(t));
+    }
+
+    @Test
+    public void testBinaryTreeRightSideViewCase2() {
+        TreeNode t = TreeNode.buildTree(new Integer[] {1, 2, 3, null, 5, null, 4});
+        Assert.assertEquals(Arrays.asList(new Integer[] {1, 3, 4}), b.rightSideView(t));
+    }
+
+    @Test
+    public void testBinaryTreeRightSideViewCase3() {
+        TreeNode t = TreeNode.buildTree(new Integer[] {1, 2, 3, 4, null, null, null});
+        Assert.assertEquals(Arrays.asList(new Integer[] {1, 3, 4}), b.rightSideView(t));
+    }
+}
