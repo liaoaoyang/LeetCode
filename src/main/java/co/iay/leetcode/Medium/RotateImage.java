ef475f1d3e33d8e9cc568fd19e259777ad187b89
liaoaoyang <liaoaoyang@gmail.com>
2017-04-15 00:04:20 +0800

    Solved Medium 48 without extra spaces

--- src/main/java/co/iay/leetcode/Medium/RotateImage.java
+++ src/main/java/co/iay/leetcode/Medium/RotateImage.java
@@ -6,17 +6,20 @@ package co.iay.leetcode.Medium;
  * https://leetcode.com/problems/rotate-image
  */
 public class RotateImage {
+    private static void swap(int[][] matrix, int i1, int j1, int i2, int j2) {
+        matrix[i1][j1] += matrix[i2][j2];
+        matrix[i2][j2] = matrix[i1][j1] - matrix[i2][j2];
+        matrix[i1][j1] = matrix[i1][j1] - matrix[i2][j2];
+    }
+
     public void rotate(int[][] matrix) {
         int matrixSize = matrix.length;
 
         for (int i = 0; i < matrixSize / 2; ++i) {
             for (int j = 0; j < (matrixSize + 1) / 2; ++j) {
-                int now = matrix[i][j];
-
-                matrix[i][j] = matrix[matrixSize - 1 - j][i];
-                matrix[matrixSize - 1 - j][i] = matrix[matrixSize - 1 - i][matrixSize - 1 - j];
-                matrix[matrixSize - 1 - i][matrixSize - 1 - j] = matrix[j][matrixSize - 1 - i];
-                matrix[j][matrixSize - 1 - i] = now;
+                swap(matrix, i, j, matrixSize - 1 - j, i); // 1<->4
+                swap(matrix, matrixSize - 1 - j, i, matrixSize - 1 - i, matrixSize - 1 - j); // 4<->3
+                swap(matrix, matrixSize - 1 - i, matrixSize - 1 - j, j, matrixSize - 1 - i);
             }
         }
     }
