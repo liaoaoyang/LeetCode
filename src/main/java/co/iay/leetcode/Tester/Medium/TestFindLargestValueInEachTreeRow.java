2abbe3fe36361c17dc1032814d1c17e756895d6e
liaoaoyang <liaoaoyang@gmail.com>
2017-04-20 22:45:36 +0800

    Add testcases

---
+++ src/main/java/co/iay/leetcode/Tester/Medium/TestFindLargestValueInEachTreeRow.java
@@ -0,0 +1,23 @@
+package co.iay.leetcode.Tester.Medium;
+
+import co.iay.leetcode.DataStructures.TreeNode;
+import co.iay.leetcode.Medium.FindLargestValueInEachTreeRow;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by ng on 17/3/26.
+ */
+public class TestFindLargestValueInEachTreeRow {
+    FindLargestValueInEachTreeRow f = new FindLargestValueInEachTreeRow();
+
+    @Test
+    public void testFindLargestValueInEachTreeRowCase1() {
+        TreeNode tree = TreeNode.buildTree(new Integer[] {1, 3, 2, 5, 3, null, 9});
+        List<Integer> cmp = Arrays.asList(new Integer[] {1, 3, 9});
+        Assert.assertEquals(cmp, f.largestValues(tree));
+    }
+}
---
+++ src/main/java/co/iay/leetcode/Tester/Medium/TestFlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,16 @@
+package co.iay.leetcode.Tester.Medium;
+
+import co.iay.leetcode.Medium.FlattenBinaryTreeToLinkedList;
+import org.junit.Test;
+
+/**
+ * Created by ng on 17/4/16.
+ */
+public class TestFlattenBinaryTreeToLinkedList {
+    private FlattenBinaryTreeToLinkedList f = new FlattenBinaryTreeToLinkedList();
+
+    @Test
+    public void testFlattenBinaryTreeToLinkedListCase1() {
+
+    }
+}
