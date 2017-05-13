5e722a43ebbfb50d94292abad01f4797e9fa3434
liaoaoyang <liaoaoyang@gmail.com>
2017-05-13 23:21:43 +0800

    Add test case

---
+++ src/main/java/co/iay/leetcode/Tester/Medium/TestKthSmallestElementInASortedMatrix.java
@@ -0,0 +1,43 @@
+package co.iay.leetcode.Tester.Medium;
+
+import co.iay.leetcode.Medium.KthSmallestElementInASortedMatrix;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by ng on 17/1/31.
+ */
+public class TestKthSmallestElementInASortedMatrix {
+    KthSmallestElementInASortedMatrix k = new KthSmallestElementInASortedMatrix();
+
+    @Test
+    public void testKthSmallestElementInASortedMatrixCase1() {
+        int[][] arr = new int[][] {
+                {1, 2},
+                {3, 3},
+        };
+
+        Assert.assertEquals(2, k.kthSmallest(arr, 2));
+    }
+
+    @Test
+    public void testKthSmallestElementInASortedMatrixCase2() {
+        int[][] arr = new int[][] {
+                {1,  5,  9},
+                {10, 11, 13},
+                {12, 13, 15},
+        };
+
+        Assert.assertEquals(13, k.kthSmallest(arr, 8));
+    }
+
+    @Test
+    public void testKthSmallestElementInASortedMatrixCase3() {
+        int[][] arr = new int[][] {
+                {1, 2},
+                {1, 3},
+        };
+
+        Assert.assertEquals(1, k.kthSmallest(arr, 2));
+    }
+}
