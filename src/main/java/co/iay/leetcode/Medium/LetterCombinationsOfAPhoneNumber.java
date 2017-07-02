e802e79892bee86b61c4731eb6247b2293aeef4d
liaoaoyang <liaoaoyang@gmail.com>
2017-07-02 23:24:39 +0800

    Solved Medium 17

---
+++ src/main/java/co/iay/leetcode/Medium/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,51 @@
+package co.iay.leetcode.Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ng on 2017/7/2.
+ * 17. Letter Combinations of a Phone Number
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+ */
+public class LetterCombinationsOfAPhoneNumber {
+    private static char[][] digitsCharsMap = new char[][] {
+            new char[] {},
+            new char[] {},
+            new char[] {'a', 'b', 'c'},
+            new char[] {'d', 'e', 'f'},
+            new char[] {'g', 'h', 'i'},
+            new char[] {'j', 'k', 'l'},
+            new char[] {'m', 'n', 'o'},
+            new char[] {'p', 'q', 'r', 's'},
+            new char[] {'t', 'u', 'v'},
+            new char[] {'w', 'x', 'y', 'z'},
+    };
+
+    private void build(String s, List<String> result, char[] c, int index) {
+        if (index == c.length) {
+            if (!s.isEmpty()) {
+                result.add(s);
+            }
+            return;
+        }
+
+        int i = c[index] - '0';
+
+        if (i <= 1) {
+            build(s, result, c, index + 1);
+            return;
+        }
+
+        for (int j = 0; j < digitsCharsMap[i].length; ++j) {
+            build(s + digitsCharsMap[i][j], result, c, index + 1);
+        }
+    }
+
+    public List<String> letterCombinations(String digits) {
+        char[] digitsChars = digits.replace("1", "").replace("0", "").toCharArray();
+        List<String> result = new ArrayList<String>();
+        build("", result, digitsChars, 0);
+        return result;
+    }
+}
---
+++ src/main/java/co/iay/leetcode/Tester/Medium/TestLetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,38 @@
+package co.iay.leetcode.Tester.Medium;
+
+import co.iay.leetcode.Medium.LetterCombinationsOfAPhoneNumber;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by ng on 2017/7/2.
+ */
+public class TestLetterCombinationsOfAPhoneNumber {
+    private LetterCombinationsOfAPhoneNumber l = new LetterCombinationsOfAPhoneNumber();
+
+    @Test
+    public void testLetterCombinationsOfAPhoneNumberCase1() {
+        Assert.assertEquals(Arrays.asList(new String[] {"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"}),
+                l.letterCombinations("23"));
+    }
+
+    @Test
+    public void testLetterCombinationsOfAPhoneNumberCase2() {
+        Assert.assertEquals(Arrays.asList(new String[] {}),
+                l.letterCombinations(""));
+    }
+
+    @Test
+    public void testLetterCombinationsOfAPhoneNumberCase3() {
+        Assert.assertEquals(Arrays.asList(new String[] {"a", "b", "c"}),
+                l.letterCombinations("2"));
+    }
+
+    @Test
+    public void testLetterCombinationsOfAPhoneNumberCase4() {
+        Assert.assertEquals(Arrays.asList(new String[] {}),
+                l.letterCombinations("111111"));
+    }
+}
