232b5b293faf601882b68084c3c6b70b25e421d6
liaoaoyang <liaoaoyang@gmail.com>
2017-07-08 17:39:29 +0800

    Add test cases

--- src/main/java/co/iay/leetcode/DataStructures/Interval.java
+++ src/main/java/co/iay/leetcode/DataStructures/Interval.java
@@ -8,4 +8,11 @@ public class Interval {
     public int end;
     public Interval() { start = 0; end = 0; }
     public Interval(int s, int e) { start = s; end = e; }
+
+    @Override
+    public boolean equals(Object obj) {
+        Interval i = (Interval) obj;
+
+        return 0 == i.start - this.start + i.end - this.end;
+    }
 }
---
+++ src/main/java/co/iay/leetcode/Tester/Medium/TestMergeIntervals.java
@@ -0,0 +1,27 @@
+package co.iay.leetcode.Tester.Medium;
+
+import co.iay.leetcode.DataStructures.Interval;
+import co.iay.leetcode.Medium.MergeIntervals;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by ng on 2017/7/7.
+ */
+public class TestMergeIntervals {
+    private MergeIntervals m = new MergeIntervals();
+
+    @Test
+    public void testMergeIntervalsCase1() {
+        Assert.assertEquals(Arrays.asList(new Interval[] {
+                new Interval(1, 6),
+                new Interval(8, 10),
+                new Interval(15, 18)}),
+                m.merge(Arrays.asList(new Interval(1, 3),
+                        new Interval(2, 6),
+                        new Interval(8, 10),
+                        new Interval(15, 18))));
+    }
+}
