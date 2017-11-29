861279546f02d4014bd2e343375800bfaeb396ee
liaoaoyang <liaoaoyang@gmail.com>
2017-11-29 23:53:05 +0800

    Add notes of Easy 69

--- src/main/java/co/iay/leetcode/Easy/SqrtX.java
+++ src/main/java/co/iay/leetcode/Easy/SqrtX.java
@@ -2,8 +2,30 @@ package co.iay.leetcode.Easy;
 
 /**
  * Created by ng on 17/2/6.
+ * 69. Sqrt(x)
+ * https://leetcode.com/problems/sqrtx/
+ * Implement int sqrt(int x).
+ * <p>
+ * Compute and return the square root of x.
+ * <p>
+ * x is guaranteed to be a non-negative integer.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 4
+ * Output: 2
+ * Example 2:
+ * <p>
+ * Input: 8
+ * Output: 2
+ * Explanation: The square root of 8 is 2.82842...,
+ * and since we want to return an integer, the decimal part will be truncated.
+ * Solution:
+ * 二分法得到平方根
  */
 public class SqrtX {
+    @SuppressWarnings("all")
     public int mySqrt(int x) {
         long middle = 0;
         long middleSquare = 0;
