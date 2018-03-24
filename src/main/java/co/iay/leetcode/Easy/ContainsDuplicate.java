ab7d30571f68b272b49764d1c12292b6940ceba9
liaoaoyang <liaoaoyang@gmail.com>
2018-03-24 23:15:55 +0800

    Add notes of some problems

--- src/main/java/co/iay/leetcode/Easy/ContainsDuplicate.java
+++ src/main/java/co/iay/leetcode/Easy/ContainsDuplicate.java
@@ -4,6 +4,14 @@ import java.util.HashMap;
 
 /**
  * Created by aoyang on 16/6/8.
+ * 217. Contains Duplicate
+ * https://leetcode.com/problems/contains-duplicate/
+ * <p>
+ * Given an array of integers, find if the array contains any duplicates.
+ * Your function should return true if any value appears at least twice in the array,
+ * and it should return false if every element is distinct.
+ * Solution:
+ * 使用HashMap解决。
  */
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
--- src/main/java/co/iay/leetcode/Easy/HammingDistance.java
+++ src/main/java/co/iay/leetcode/Easy/HammingDistance.java
@@ -2,6 +2,31 @@ package co.iay.leetcode.Easy;
 
 /**
  * Created by ng on 16/12/23.
+ * 461. Hamming Distance
+ * https://leetcode.com/problems/hamming-distance/
+ * <p>
+ * The Hamming distance between two integers is the number of positions
+ * at which the corresponding bits are different.
+ * <p>
+ * Given two integers x and y, calculate the Hamming distance.
+ * <p>
+ * Note:
+ * 0 ≤ x, y < 231.
+ * <p>
+ * Example:
+ * <p>
+ * Input: x = 1, y = 4
+ * <p>
+ * Output: 2
+ * <p>
+ * Explanation:
+ * 1   (0 0 0 1)
+ * 4   (0 1 0 0)
+ * ↑   ↑
+ * <p>
+ * The above arrows point to positions where the corresponding bits are different.
+ * Solution:
+ * 转化为二进制字符串，统计1的个数即可。
  */
 public class HammingDistance {
     public int hammingDistance(int x, int y) {
