360fc9b6633ad3718c7d7c96946ab337d6f6e246
liaoaoyang <liaoaoyang@gmail.com>
2018-06-16 23:55:13 +0800

    Solved Easy 844

---
+++ src/main/java/co/iay/leetcode/Easy/BackspaceStringCompare.java
@@ -0,0 +1,61 @@
+package co.iay.leetcode.Easy;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * 844. Backspace String Compare
+ * https://leetcode.com/problems/backspace-string-compare/
+ * Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: S = "ab#c", T = "ad#c"
+ * Output: true
+ * Explanation: Both S and T become "ac".
+ * Example 2:
+ * <p>
+ * Input: S = "ab##", T = "c#d#"
+ * Output: true
+ * Explanation: Both S and T become "".
+ * Example 3:
+ * <p>
+ * Input: S = "a##c", T = "#a#c"
+ * Output: true
+ * Explanation: Both S and T become "c".
+ * Example 4:
+ * <p>
+ * Input: S = "a#c", T = "b"
+ * Output: false
+ * Explanation: S becomes "c" while T becomes "b".
+ * Note:
+ * <p>
+ * 1 <= S.length <= 200
+ * 1 <= T.length <= 200
+ * S and T only contain lowercase letters and '#' characters.
+ * Follow up:
+ * <p>
+ * Can you solve it in O(N) time and O(1) space?
+ */
+public class BackspaceStringCompare {
+    private String str2stack2str(String S) {
+        Stack<Character> ss = new Stack<>();
+
+        for (int i = 0; i < S.length(); ++i) {
+            if (S.charAt(i) == '#') {
+                if (!ss.isEmpty()) {
+                    ss.pop();
+                }
+                continue;
+            }
+
+            ss.push(S.charAt(i));
+        }
+
+        return Arrays.toString(ss.toArray());
+    }
+
+    public boolean backspaceCompare(String S, String T) {
+        return str2stack2str(S).equals(str2stack2str(T));
+    }
+}
