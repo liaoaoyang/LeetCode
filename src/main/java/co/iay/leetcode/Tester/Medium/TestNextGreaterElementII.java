1f3d756574b337b1e00a4a3ab59ca6a13480306f
liaoaoyang <liaoaoyang@gmail.com>
2018-11-18 00:19:12 +0800

    Add test case

--- src/main/java/co/iay/leetcode/Tester/Medium/TestNextGreaterElementII.java
+++ src/main/java/co/iay/leetcode/Tester/Medium/TestNextGreaterElementII.java
@@ -11,4 +11,9 @@ public class TestNextGreaterElementII {
     public void testNextGreaterElementIICase1() {
         Assert.assertArrayEquals(new int[]{2, -1, 2}, n.nextGreaterElements(new int[]{1, 2, 1}));
     }
+
+    @Test
+    public void testNextGreaterElementIICase2() {
+        Assert.assertArrayEquals(new int[]{-1, 5, 5, 5, 5}, n.nextGreaterElements(new int[]{5, 4, 3, 2, 1}));
+    }
 }
